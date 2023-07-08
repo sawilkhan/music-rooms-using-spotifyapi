c6e62c40a53caeff9878eb6be65da674387a2b7b
sawilkhan <68275664+sawilkhan@users.noreply.github.com>
2023-07-08 12:35:13 +0530

    Initial Commint
    Uploaded project files

---
+++ comusic/spotify/credentials.py
@@ -0,0 +1,8 @@
+# CLIENT_ID = "cedbd96291884e5991176acc8ab8226b"
+# CLIENT_SECRET = "5bab246ae8fd4d52a672c5c82d2e513c"
+# REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+
+CLIENT_ID = "def29b79d58e414ca9628970e2fbed0b"
+CLIENT_SECRET = "81aa093c0b53497a9ab15576b7195ce9"
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+
